0640e76f77bd76eea1fc265ede91b3b3a0bd7677
brusatimatias <mformento8@gmail.com>
2026-09-16 14:25:58 -0300

    Google credentials added

--- src/auth.ts
+++ src/auth.ts
@@ -36,6 +36,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: "jwt" },
   providers: [
     Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           access_type: "offline",
